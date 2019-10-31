c3d9f12adbd6f054501487af886ecbbb8db588ab
Connie <constanceli@ku.edu>
2019-10-31 13:58:33 -0500

    feat(GUI): add GUI & Config classes

--- src/gui.js
+++ src/gui.js
@@ -1,2 +1,19 @@
 /* gui.js */
 
+class GUI {
+	m_config;
+	constructor() {
+		this.m_config = new Config();
+	}
+
+
+	/** Based on the selection of one dropdown menu, populate the options of another dropdown menu.
+	 * @param {string} menuA - the elementID of the first menu
+	 * @param {string} menuB - the elementID of the second menu
+	 */
+	function populateDropdown(menuA, menuB) {
+		let optionA = document.getElementById(menuA).value;
+
+	}
+
+}
