e2f25ab5f1dda263a4c56171a5402d004027a217
Kyle Kappes-Sum <kylekappessum@gmail.com>
2019-11-01 13:33:23 -0500

    feat(formulaAbs): multiplySolve
    Added mass multiply and divide function

---
+++ src/formulasAbs.js
@@ -0,0 +1,17 @@
+class formulasAbs{
+    multiplySolve(numer, denom){
+        let numerTotal = 1;
+        let denomTotal = 1;
+        if(numer.length > 0){
+            for(let i = 0; i < numer.length; i++){
+                numerTotal = numerTotal * numer[i];
+            }
+        }
+        if(denom.length > 0){
+            for(let i = 0; i < denom.length; i++){
+                denomTotal = denomTotal * denom[i];
+            }
+        }
+        return numerTotal/denomTotal;
+    }
+}
