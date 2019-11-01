9fc1220dd2d2fc8633960bd90de04bfaeb6ce0a7
Connie <constanceli@ku.edu>
2019-10-31 22:52:23 -0500

    feat(GUI): add populateDropdown method

--- src/gui.js
+++ src/gui.js
@@ -6,14 +6,22 @@ class GUI {
 	 */
 	
 	
-
 	/** Based on the selection of one dropdown menu, populate the options of another dropdown menu.
-	 * @param {string} menuA - the elementID of the first menu
-	 * @param {string} menuB - the elementID of the second menu
+	 * @param {string} menuAId - the elementID of the first menu
+	 * @param {string} menuBId - the elementID of the second menu
 	 */
-	function populateDropdown(menuA, menuB) {
-		let optionA = document.getElementById(menuA).value;
-
+	populateNextDropdown(menuAId, menuBId) {
+		let menuB = document.getElementById(menuBId);
+		menuB.innerHTML = "";
+		let menuA = document.getElementById(menuAId);
+		let optionA = menuA.value;
+		let tempStr = "";
+			
+		CONFIG.ENERGY_UNITS.forEach(function(item) {
+			if(item[0] != optionA) {
+				menuB.innerHTML += "<option value=\"" + item[0] + "\">" + item[1] + "</option>";
+			}
+		});
 	}
 
 }
