e78faa5c58bf70a0b95d0809cd9274f61f71b1fa
Kyle Kappes-Sum <kylekappessum@gmail.com>
2019-11-02 00:33:15 -0500

    feat: increased content

--- src/formulaSol.js
+++ src/formulaSol.js
@@ -1,11 +1,32 @@
 class formulaSol{
     uConst;
-    
-    constructor(){
+    fAbs;
 
+    constructor(){
+        fAbs = new formulasAbs();
     }
 
     pvNRT(obj){
+        //todo
+        let returnable = 0;
+        let tempArr = ['p', 'v', 'n', 't'];
+        let variable = fAbs.findVar(obj, tempArr);
+        switch(variable){
+            case 'p':
+                
+                break;
+            case 'v':
+                
+                break;
+            case 'n':
+                
+                break;
+            case 't':
 
+                break;
+            default:
+                console.log("Error in PVNRT: Bad var");
+        }
+        return returnable;
     }
 }
--- src/formulasAbs.js
+++ src/formulasAbs.js
@@ -3,6 +3,23 @@ class formulasAbs{
         
     }
 
+    findVar(obj, arr){
+        let objArr = Object.getOwnPropertyNames(obj);
+        let count = 0;
+        for(let i = 0; i < arr.length; i++){
+            count = 0;
+            let tempArr = new Array(objArr.length - 1);
+            for(let j = 0; j < objArr.length; j++){
+                if(arr[i] == objArr[j]){
+                    tempArr[count] = objArr[j];
+                    count++;
+                }
+            }
+            objArr = tempArr;
+        }
+        return objArr[0];
+    }
+
     multiplySolve(numer, denom){
         let numerTotal = 1;
         let denomTotal = 1;
