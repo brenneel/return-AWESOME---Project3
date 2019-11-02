76ef35fae7ecb30571f83527c8fc49aa93b64e78
Kyle Kappes-Sum <kylekappessum@gmail.com>
2019-11-02 16:18:13 -0500

    feat(unitConstants): Added Univ gas constant

--- src/unitConstant.js
+++ src/unitConstant.js
@@ -3,7 +3,17 @@ class unitConstants{
 
     }
 
-    MATERIAL_CONST={
-        UNIV_GAS_CONST: ""
+    UNIV_GAS_CONST={
+        BTU_LBMOL_R:            1.987,
+        CAL_MOL_K:              1.987,
+        KCAL_KGMOL_K:           1.987,
+        J_MOL_K:                8.314,
+
+        M3_PA_MOL_K:            8.314,
+        L_ATM_MOL_K:            0.08206,
+        L_BAR_MOL_K:            0.08314,
+
+        LFB_IN2_FT3_LBMOL_R:    10.73,
+        ATM_FT3_LBMOL_R:        0.7302,
     }
 }
