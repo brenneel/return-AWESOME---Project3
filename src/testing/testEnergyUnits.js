45eea8a7e303c8197a46dfc6c47b77f44de615c2
Malena Schoeni <mschoeni789@gmail.com>
2019-11-11 18:54:04 -0600

    feat: add testEnergyUnits.js

---
+++ src/testing/testEnergyUnits.js
@@ -0,0 +1,13 @@
+class TestEnergyUnits extends Test{
+    constructor(){
+        super("EnergyUnits");
+        // this.className = "FormulasAbs";
+    }
+
+    run(){
+        this.addTest("test2");
+        this.updateTest("test2", this.pass);
+        this.conLog("test3", "testing");
+    }
+
+}
