fe1b44325d1486dfc9245221d942ceb1de510e3e
Kyle Kappes-Sum <kylekappessum@gmail.com>
2019-11-18 14:42:18 -0600

    feat(FormulaAbs): Removed old html file

--- src/testing/testFormulasAbs.js
+++ src/testing/testFormulasAbs.js
@@ -1,11 +1,65 @@
 class TestFormulasAbs extends Test{
+    fAbs;
+
     constructor(){
         super("FormulasAbs");
+        this.fAbs = new FormulasAbs;
+        
     }
 
     run(){
-        this.addTest("test2");
-        this.updateTest("test2", this.pass);
-        this.conLog("test3", "testing");
+        this.createTest(this.testSum(), "Testing Sum");
+        this.createTest(this.testDifference(), "Testing Difference");
+        this.createTest(this.testProduct(), "Testing Product");
+        this.createTest(this.testQuotient(), "Testing Quotient");
+        
+    }
+
+    testSum(){
+        let arr = new Array();
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        if(this.fAbs.sum(arr) == 9){
+            return this.pass;
+        }else{
+            return this.unkno;
+        }
+    }
+
+    testDifference(){
+        let arr = new Array();
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        if(this.fAbs.difference(arr) == -9){
+            return this.pass;
+        }else{
+            return this.unkno;
+        }
+    }
+
+    testProduct(){
+        let arr = new Array();
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        if(this.fAbs.product(arr) == 24){
+            return this.pass;
+        }else{
+            return this.unkno;
+        }
+    }
+
+    testQuotient(){
+        let arr = new Array();
+        arr.push(2);
+        arr.push(3);
+        arr.push(4);
+        if(this.fAbs.quotient(arr) == 1/24){
+            return this.pass;
+        }else{
+            return this.unkno;
+        }
     }
 }
