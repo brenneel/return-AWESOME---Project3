2a1f9298076e72a9b2559f142e094b9fa08644f9
Ethan <ethanlbrenner@gmail.com>
2019-11-20 17:03:09 -0600

    feat: filled out run()

--- src/testing/testViscosityUnits.js
+++ src/testing/testViscosityUnits.js
@@ -13,8 +13,43 @@ class TestViscosityUnits extends Test {
 
     run(){
         //addtest
-
+        //let temp = Object.getOwnPropertyNames(this.viscos);
+        //console.log(temp);
         //updateTest
+
+        this.addTest("cPtoP");
+        this.addTest("pToCP");
+        this.addTest("cPtoGramCentiSec");
+        this.addTest("gramCentiSecToCP");
+        this.addTest("cPtoDyne");
+        this.addTest("dyneToCP");
+        this.addTest("cPtoN");
+        this.addTest("NtoCP");
+        this.addTest("cPtoPaS");
+        this.addTest("paSToCP");
+        this.addTest("cPtoKgMs");
+        this.addTest("kgMsToCP");
+        this.addTest("cPtolbmFtS");
+        this.addTest("lbmFtStoCP");
+        this.addTest("cPtolbfSft");
+        this.addTest("lbfSftToCP");
+
+        this.updateTest("cPtoP", testingCPtoP(0));
+        this.updateTest("pToCP", testingPToCP(1));
+        this.updateTest("cPtoGramCentiSec", testingCPtoGramCentiSec(2));
+        this.updateTest("gramCentiSecToCP", testingGramCentiSecToCP(3));
+        this.updateTest("cPtoDyne", testingCPtoDyne(4));
+        this.updateTest("dyneToCP", testingDyneToCP(5));
+        this.updateTest("cPtoN", testingCPtoN(6));
+        this.updateTest("NtoCP", testingNtoCP(7));
+        this.updateTest("cPtoPaS", testingCPtoPaS(8));
+        this.updateTest("paSToCP", testingPaSToCP(9));
+        this.updateTest("cPtoKgMs", testingCPtoKgMs(10));
+        this.updateTest("kgMsToCP", testingKgMsToCP(11));
+        this.updateTest("cPtolbmFtS", testingCPtolbmFtS(12));
+        this.updateTest("lbmFtStoCP", testingLbmFtStoCP(13));
+        this.updateTest("cPtolbfSft", testingCPtolbfSft(14));
+        this.updateTest("lbfSftToCP", testingLbfSftToCP(15));
     }
 
     
