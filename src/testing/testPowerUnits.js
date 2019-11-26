521b84ff99211bed3fe20749f91d76792ad4a783
Ethan <ethanlbrenner@gmail.com>
2019-11-25 18:14:39 -0600

    updated tolerance level

--- src/testing/testPowerUnits.js
+++ src/testing/testPowerUnits.js
@@ -7,7 +7,7 @@ class TestPowerUnits extends Test{
         super("PowerUnits");
         this.testValues = [0.000000001234, 1024.123456, 987654];
         this.expectedValues = [];
-        this.tolerance = 0.001 //this is in percent
+        this.tolerance = 0.0001 //this is in percent
         this.power = new PowerUnits();
         
     }
