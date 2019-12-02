d5197d21aff4173256df39d257efcbbc898a9f26
Connie <constanceli@ku.edu>
2019-12-01 19:21:06 -0600

    feat(Test/Calc): add testCalculator.js

---
+++ src/testing/testCalculator.js
@@ -0,0 +1,23 @@
+/** Tests for {@link Calculator} class methods.
+*/
+class TestCalculator extends Test {
+	constructor() {
+		super("Calculator");
+		this.CALC = new Calculator();
+	}
+	
+	/** Runs all tests for {@link Calculator}.
+	 */
+	run() {
+		
+	}
+	
+	/** Tests {@link Calculator}'s genConversionID method.
+	 */
+	testGenConversionID() {
+		
+	}
+	
+	/** Tests {@link Calculator}'s method.
+	 */
+}
