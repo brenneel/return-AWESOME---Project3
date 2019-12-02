524535dc48a99c5c17dd92b966431632cc9e6866
Ethan <ethanlbrenner@gmail.com>
2019-12-01 21:34:00 -0600

    feat: added run() boilerplate

--- src/testing/testGui.js
+++ src/testing/testGui.js
@@ -12,12 +12,34 @@ class TestGui extends Test {
 		this.GUI.initialize();
 		let arr = new Array();
         let count = 0;
-        let result = "";
+		let result = "";
+		
+		arr.push(this.createTest(this.testingCopyValue), "copyValue");
+
+		for(let i = 0; i < arr.length; i++){
+            if(arr[i] == this.pass){
+                count++;
+            }
+        }
+
+        if(count == arr.length){
+            result = this.pass;
+        }else{
+            result = this.fail;
+        }
+        this.updateTestSet(result, count, arr.length);
+        return result;
 	}
 	
 	/* NOTE: all Gui methods that only manipulate some aspect of the webpage's display (for example, populating a dropdown menu) were tested manually and thus are not covered by this test suite.
 	*/
 	
-	
+	/**
+	 * checks whether the value was correctly copied to clipboard
+	 * @post the value is stored in the user's clipboard
+	 */
+	testingCopyValue(){
+
+	}
 }
 
