b36c841c73e0f1180b4cc7c2d622e58f47e262a6
Ethan <ethanlbrenner@gmail.com>
2019-12-01 22:59:57 -0600

    feat: testValCSNs implemented

--- src/testing/testGui.js
+++ src/testing/testGui.js
@@ -12,10 +12,10 @@ class TestGui extends Test {
         let count = 0;
 		let result = "";
 		
-		arr.push(this.createTest(this.testingCopyValue(), "copyValue"));
+		arr.push(this.createTest(this.testingCopyValue(), "Testing copyValue()"));
 		arr.push(this.createTest(this.testValOneEmpty(), "Testing valOneEmpty()"));
 		arr.push(this.createTest(this.testValNoneEmpty(), "Testing valNoneEmpty()"));
-
+		arr.push(this.createTest(this.testValCSNs(), "Testing ValCSNs()"));
 		for(let i = 0; i < arr.length; i++){
             if(arr[i] == this.pass){
                 count++;
@@ -115,7 +115,29 @@ class TestGui extends Test {
 	/** Tests {@link Gui}'s valCSNs() method.
 	 */
 	testValCSNs() {
-		
+		let valueField = document.getElementById("textHelperBox");
+		let passed1 = false;
+		let passed2 = false;
+		let passed3 = false;
+		valueField.value = "1,2,3,4";
+		if(GUI.valCSNs("textHelperBox")){
+			passed1 = true;
+			
+		}
+		valueField.value = "1 2 3 4";
+		if(GUI.valCSNs("textHelperBox")){
+			return(this.fail);
+		}
+		passed2 = true;
+		valueField.value = "asdf";
+		if(GUI.valCSNs("textHelperBox")){
+			return(this.fail);
+		}
+		passed3 = true;
+		if(passed1 == passed2 && passed2 == passed3 && passed3 == true){
+			return(this.pass);
+		}
+		return(this.fail);
 	}
 	
 	/** Tests {@link Gui}'s inputsEmpty() method.
